4d2e9720cf9cca006a1c6d76b10dca584682caa2
gaixiaoyang <gaixiaoyang@163.com>
2014-08-11 11:36:32 +0800

    codes commit

--- Bmob_IM_test/src/com/bmob/im/demo/ui/DetailActivity.java
+++ Bmob_IM_test/src/com/bmob/im/demo/ui/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.bmob.im.demo.ui;
 
 import android.os.*;
+import android.view.*;
 import android.widget.*;
 
 import com.bmob.im.demo.*;
@@ -19,6 +20,8 @@ public class DetailActivity extends BaseActivity {
 	private TextView detail_time = null;
 	private TextView detail_place = null;
 	private TextView detail_content = null;
+	private Button btn_save = null;
+	private Button btn_check = null;
 	private String from = "";
 
 	@Override
@@ -35,12 +38,20 @@ public class DetailActivity extends BaseActivity {
 		detail_place = (TextView)findViewById(R.id.detail_place);
 		detail_content = (TextView)findViewById(R.id.detail_content);
 		
+		btn_save = (Button)findViewById(R.id.btn_save);
+		btn_check = (Button)findViewById(R.id.btn_check);
+		
 		detail_time.setText("2014-08-08 12:10");
 		detail_place.setText("北京市朝阳区花家地望京小腰");
 		detail_content.setText("欢迎前来\n欢迎美女\n欢迎汉子");
 		
 		if(from.equals("mylaunch")){
-			
+			btn_save.setVisibility(View.INVISIBLE);
+		}else if(from.equals("myjoin")){
+			btn_save.setVisibility(View.INVISIBLE);
+			btn_check.setVisibility(View.INVISIBLE);
+		}else if(from.equals("activity")){
+			btn_check.setVisibility(View.INVISIBLE);
 		}
 	}
 	
