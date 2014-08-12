0475fae5398a24aa6704d4dceed99a68dbe7dab4
gaixiaoyang <gaixiaoyang@163.com>
2014-08-12 18:21:48 +0800

    add time picker dialog

--- Bmob_IM_test/src/com/bmob/im/demo/ui/fragment/LaunchFragment.java
+++ Bmob_IM_test/src/com/bmob/im/demo/ui/fragment/LaunchFragment.java
@@ -1,11 +1,13 @@
 package com.bmob.im.demo.ui.fragment;
 
+import java.util.*;
+
 import android.annotation.*;
+import android.app.*;
 import android.os.*;
 import android.view.*;
+import android.view.View.OnClickListener;
 import android.widget.*;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 
 import com.bmob.im.demo.*;
 import com.bmob.im.demo.ui.*;
@@ -19,29 +21,45 @@ import com.bmob.im.demo.ui.*;
  * @date 2014-6-7 下午1:02:05
  */
 @SuppressLint("DefaultLocale")
-public class LaunchFragment extends FragmentBase implements OnItemClickListener, OnItemLongClickListener {
+public class LaunchFragment extends FragmentBase implements OnClickListener {
+
+	private EditText tv_name;
+	private TextView tv_time;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_launch, container, false);
 	}
-	
+
 	@Override
 	public void onActivityCreated(android.os.Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		initTopBarForOnlyTitle("发起请客");
-		
-	};
-
-	@Override
-	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-		return false;
+		tv_name = (EditText) findViewById(R.id.tv_name);
+		tv_time = (TextView) findViewById(R.id.tv_time);
+		tv_name.setOnClickListener(this);
+		tv_time.setOnClickListener(this);
+         
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.tv_name:
+			break;
+		case R.id.tv_time:
+			showTimeDialog();
+			break;
+		};
 	}
-
+	
+	private void showTimeDialog(){
+		 Calendar c = Calendar.getInstance();  
+		 new DatePickerDialog(LaunchFragment.this.getActivity(), new DatePickerDialog.OnDateSetListener() {  
+		     @Override  
+		     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {  
+		   	  tv_time.setText(year+"/"+(monthOfYear+1)+"/"+dayOfMonth);  
+		     }  
+		 }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();  
+	}
 }
